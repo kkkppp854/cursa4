77acc41a3e46001e7476d1db04acf9979f69ad7c
kkkppp854 <you@gmail.com>
2025-06-20 10:24:45 +0300

    deploy fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/cursa4/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-})
+  plugins: [react()],
+});
